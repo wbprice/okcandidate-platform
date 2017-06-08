835749fc3ff1701a8aa40e4cd268900e1ce06b66
Blaine Price <1wbprice@gmail.com>
2017-06-08 13:31:04 -0400

    debug

--- frontend/js/components/ecosystems/CategoryList.js
+++ frontend/js/components/ecosystems/CategoryList.js
@@ -1,25 +1,67 @@
 import React, { PropTypes, Component } from 'react';
+import MultiBackend, { Preview } from 'react-dnd-multi-backend';
+import HTML5toTouch from 'react-dnd-multi-backend/lib/HTML5toTouch';
+import { DragDropContext } from 'react-dnd';
+import update from 'immutability-helper';
 
 import CategoryListItem from './../organisms/CategoryListItem';
 
 class CategoryList extends Component {
 
-    render() {
+    constructor(props) {
+        super(props);
+        this.moveCard = this.moveCard.bind(this);
+        this.state = {
+            categories: []
+        };
+    }
 
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            categories: nextProps.categories
+        });
+    }
+
+    generatePreview(type, item, style) {
         return (
-      <div className="category-list">
-        {
-          this.props.categories.map((categoryItem, index) => {
-              return (<CategoryListItem
-              key={index}
-              name={categoryItem.name}
-              icon={categoryItem.icon}
-              rank={categoryItem.rank} />);
-          })
-        }
-      </div>
-    );
+            <div style={style}>
+                <pre>Generated Card</pre>
+            </div>
+        );
+    }
 
+    moveCard(dragIndex, hoverIndex) {
+        const {categories} = this.state;
+        const dragCategory = categories[dragIndex];
+
+        this.setState(update(this.state, {
+            categories: {
+                $splice: [
+                    [dragIndex, 1],
+                    [hoverIndex, 0, dragCategory]
+                ]
+            }
+        }));
+    }
+
+    render() {
+        return (
+            <div className="category-list">
+                <Preview generator={this.generatePreview.bind(this)} />
+                {
+                this.state.categories.map((categoryItem, index) => {
+                    return (
+                        <CategoryListItem
+                            key={index}
+                            moveCard={this.moveCard}
+                            name={categoryItem.name}
+                            icon={categoryItem.icon}
+                            rank={categoryItem.rank} />
+                    );
+                })
+                }
+            </div>
+        );
     }
 
 }
@@ -28,4 +70,4 @@ CategoryList.propTypes = {
     categories: PropTypes.array
 };
 
-export default CategoryList;
+export default DragDropContext(MultiBackend(HTML5toTouch))(CategoryList);
--- frontend/js/components/organisms/CategoryListItem.js
+++ frontend/js/components/organisms/CategoryListItem.js
@@ -1,6 +1,66 @@
 import React, { PropTypes, Component } from 'react';
-
+import { findDOMNode } from 'react-dom';
+import { DragSource, DropTarget } from 'react-dnd';
 import Icon from './../atoms/Icon';
+
+const ItemTypes = {
+    CARD: 'card'
+};
+
+const cardSource = {
+    beginDrag(props) {
+        return {
+            id: props.id,
+            index: props.index
+        };
+    }
+};
+
+const cardTarget = {
+    hover(props, monitor, component) {
+        const dragIndex = monitor.getItem().index;
+        const hoverIndex = props.index;
+
+        if (dragIndex == hoverIndex) {
+            return;
+        }
+
+        // bounding rect
+        const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+
+        // vertical middle
+        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+        // mouse offset
+        const clientOffset = monitor.getClientOffset();
+
+        // get pixels to top
+        const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+        // Only perform the move when the mouse has crossed half of the items height
+        // When dragging downwards, only move when the cursor is below 50%
+        // When dragging upwards, only move when the cursor is above 50%
+
+        // Dragging downwards
+        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+            return;
+        }
+
+        // Dragging upwards
+        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+            return;
+        }
+
+        // Time to actually perform the action
+        props.moveCard(dragIndex, hoverIndex);
+
+        // Note: we're mutating the monitor item here!
+        // Generally it's better to avoid mutations,
+        // but it's good here for the sake of performance
+        // to avoid expensive index searches.
+        monitor.getItem().index = hoverIndex;
+    }
+};
 
 class CategoryListItem extends Component {
 
@@ -9,7 +69,13 @@ class CategoryListItem extends Component {
     }
 
     render() {
-        return (
+
+        const {
+            connectDragSource,
+            connectDropTarget
+        } = this.props;
+
+        return connectDragSource(connectDropTarget(
             <div className="category-list-item">
                 <div className="rank">
                     <label>{this.props.rank}</label>
@@ -23,15 +89,26 @@ class CategoryListItem extends Component {
                     <span className="category-name-label">{this.props.name}</span>
                 </div>
           </div>
-        );
+        ))
     }
-
 }
 
 CategoryListItem.propTypes = {
+    connectDragSource: PropTypes.func.isRequired,
+    connectDropTarget: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired,
     name: PropTypes.string,
     icon: PropTypes.string,
     rank: PropTypes.number
 };
 
-export default CategoryListItem;
+const DragCategoryListItem = DragSource(ItemTypes.CARD, cardSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+}))(CategoryListItem);
+
+const DragDropCategoryListItem = DropTarget(ItemTypes.CARD, cardTarget, (connect) => ({
+    connectDropTarget: connect.dropTarget()
+}))(DragCategoryListItem);
+
+export default DragDropCategoryListItem;
