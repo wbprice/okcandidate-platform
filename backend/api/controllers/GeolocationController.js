d6343c5a184b8ca5daa8f327c47711e70a906c0b
Blaine Price <1wbprice@gmail.com>
2018-03-27 21:33:31 -0400

    Use simple custom server

--- backend/api/controllers/GeolocationController.js
+++ backend/api/controllers/GeolocationController.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict'
 
-const Controller = require('trails-controller');
-const fetch = require('node-fetch');
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-const geocodingUrl = 'https://maps.google.apis.com/maps/api/geocode/json';
+const Controller = require('trails-controller')
+const fetch = require('node-fetch')
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+const geocodingUrl = 'https://maps.google.apis.com/maps/api/geocode/json'
 
 /**
  * @module GeolocationController
@@ -11,18 +11,18 @@ const geocodingUrl = 'https://maps.google.apis.com/maps/api/geocode/json';
  */
 module.exports = class GeolocationController extends Controller {
 
-    get(request, reply) {
-        const address = JSON.stringify(request.query.address);
+  get(request, reply) {
+    const address = JSON.stringify(request.query.address)
 
-        fetch(`${geocodingUrl}?address=${address}&key=${GOOGLE_API_KEY}`)
+    fetch(`${geocodingUrl}?address=${address}&key=${GOOGLE_API_KEY}`)
             .then((response) => response.json())
             .then((response) => {
-                reply(response);
+              reply(response)
             })
             .catch((error) => {
-                reply(error);
-            });
-    }
+              reply(error)
+            })
+  }
 
-};
+}
 
