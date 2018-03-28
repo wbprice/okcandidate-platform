91af043cd972ad86fc2baf564bef8fbf80a9dcbc
Blaine Price <1wbprice@gmail.com>
2018-03-27 22:02:31 -0400

    -_____-

--- frontend/index.js
+++ frontend/index.js
@@ -4,29 +4,27 @@
 // Make sure the syntax and sources this file requires are compatible with the current node version you are running
 // See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
 
-const { createServer } = require('http')
+const express = require('express');
+const exp = express();
+const proxy = require('http-proxy-middleware');
 const { parse } = require('url')
 const next = require('next')
+const path = require('path');
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
-    console.log('pathname: ' + pathname);
-    if (pathname.includes('api')) {
-      console.log('API: this is an API route!');
-      res.end('API endpoint');
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  }).listen(3000, err => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
+
+  exp.all('/api*', proxy('http://0.0.0.0:8080/api'));
+
+  exp.get('/*', (req, res) => {
+    return handle(req, res, parse(req.url, true));
+  });
+
+  exp.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('Listening on 3000');
+  });
 })
